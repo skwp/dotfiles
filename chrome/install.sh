29f8403ed9b73cc7f5893ae2173f5167611be952
yan <yan@pritzker.ws>
2012-01-22 20:40:44 -0800

    Chrome config: make web inspector fonts bigger

---
+++ chrome/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Fix chrome web inspector fonts to be readable.
+# http://blog.dotsmart.net/2011/09/30/change-font-size-in-chrome-devtools/
+
+# Use:
+cp ~/.dotfiles/chrome/Custom.css ~/Library/Application\ Support/Google/Chrome/Default/Custom.css
