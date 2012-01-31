228d89ec38c8cb73a3637946f1e87d0b6e4b034b
Kyle West <kswest@gmail.com>
2012-01-30 23:46:41 -0500

    added zsh aliases. Moved other files but left symlinks for backwards compatibility.

---
+++ zsh/aliases.zsh
@@ -0,0 +1,119 @@
+# Aliases in this file are bash and zsh compatible
+
+# Don't change. The following determines where YADR is installed.
+yadr=`find -L ~ -type file -maxdepth 2 -name .yadr | head | sed 's:\.yadr\/::'`
+
+# YADR support
+alias yav='yadr vim-add-plugin'
+alias yuv='yadr vim-update-plugins'
+alias yip='yadr init-plugins'
+
+# PS
+alias psg="ps aux | grep "
+alias psr='ps aux | grep ruby'
+
+# Moving around
+alias ..='cd ..'
+alias cdb='cd -'
+
+# Show human friendly numbers and colors
+alias df='df -h'
+alias ll='ls -alGh'
+alias ls='ls -Gh'
+alias du='du -h -d 2'
+
+# show me files matching "ls grep"
+alias lsg='ll | grep'
+
+# Alias Editing
+alias ae='vi $yadr/zsh/aliases' #alias edit
+alias ar='source $yadr/zsh/aliases'  #alias reload
+
+# vimrc editing
+alias ve='vi ~/.vimrc'
+
+# zsh profile editing
+alias ze='vi ~/.zshrc'
+alias zr='source ~/.zshrc'
+
+# Git Aliases
+alias gs='git status'
+alias gstsh='git stash'
+alias gst='git stash'
+alias gsh='git show'
+alias gshw='git show'
+alias gshow='git show'
+alias gi='vi .gitignore'
+alias gcm='git ci -m'
+alias gcim='git ci -m'
+alias gci='git ci'
+alias gco='git co'
+alias ga='git add -A'
+alias guns='git unstage'
+alias gunc='git uncommit'
+alias gm='git merge'
+alias gms='git merge --squash'
+alias gam='git amend'
+alias gr='git rebase'
+alias gra='git rebase --abort'
+alias grc='git rebase --continue'
+alias gbi='git rebase --interactive'
+alias gl='git l'
+alias glg='git l'
+alias glog='git l'
+alias co='git co'
+alias gf='git fetch'
+alias gfch='git fetch'
+alias gd='git diff'
+alias gb='git b'
+alias gdc='git diff --cached'
+alias gpub='grb publish'
+alias gtr='grb track'
+alias gpl='git pull'
+alias gplr='git pull --rebase'
+alias gps='git push'
+alias gpsh='git push'
+alias gnb='git nb' # new branch aka checkout -b
+alias grs='git reset' 
+alias grsh='git reset --hard'
+alias gcln='git clean'
+alias gclndf='git clean -df'
+alias gsm='git submodule'
+alias gsmi='git submodule init'
+alias gsmu='git submodule update'
+alias gt='git t'
+
+# Common shell functions
+alias less='less -r'
+alias tf='tail -f'
+alias l='less'
+alias lh='ls -alt | head' # see the last modified files
+alias screen='TERM=screen screen'
+alias cl='clear'
+alias ps='ps aux'
+
+# Zippin
+alias gz='tar -zcvf'
+
+# Ruby
+alias c='script/console --irb=pry'
+alias ms='mongrel_rails start'
+
+# Vim/ctags "mctags = make ctags", using the ruby specific version
+# to save some time
+alias mctags=~/.bin/run_tags.rb #'/opt/local/bin/ctags -Rf ./tags *'
+
+alias ka9='killall -9'
+alias k9='kill -9'
+
+# This trick makes sudo understand all my aliases
+alias sudo='sudo '
+
+# Gem install
+alias sgi='sudo gem install --no-ri --no-rdoc'
+
+# TODOS
+# This uses NValt (NotationalVelocity alt fork) - http://brettterpstra.com/project/nvalt/
+# to find the note called 'todo'
+alias todo='open nvalt://find/todo'
+
