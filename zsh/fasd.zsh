1aa9e5b87581d784688cdd711f332b74f0c45877
Kyle West <kswest@gmail.com>
2012-03-22 16:15:42 -0400

    Only init fasd if it is installed.

--- zsh/fasd.zsh
+++ zsh/fasd.zsh
@@ -1,13 +1,16 @@
-# Initialize fasd (https://github.com/clvv/fasd)
-eval "$(fasd --init posix-alias zsh-hook)"
+# only init if installed.
+if [[ -d $HOME/.fasd ]]; then
+  # Initialize fasd (https://github.com/clvv/fasd)
+  eval "$(fasd --init posix-alias zsh-hook)"
 
-# aliases
+  # aliases
 
-# jump to recently used items
-alias a='fasd -a' # any
-alias s='fasd -s' # show / search / select
-alias d='fasd -d' # directory
-alias f='fasd -f' # file
-alias z='fasd_cd -d' # cd, same functionality as j in autojump
-alias v='f -e vim' # quick opening files with vim
+  # jump to recently used items
+  alias a='fasd -a' # any
+  alias s='fasd -s' # show / search / select
+  alias d='fasd -d' # directory
+  alias f='fasd -f' # file
+  alias z='fasd_cd -d' # cd, same functionality as j in autojump
+  alias v='f -e vim' # quick opening files with vim
+fi
 
