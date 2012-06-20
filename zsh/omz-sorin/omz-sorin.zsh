e09a0942ca2819b7c3f735f10c12cab5c82346f4
Pete Schaffner <pjschaffner@gmail.com>
2012-06-20 13:01:16 -0400

    Fixing omodload: no such module: alias issue (#111)

--- zsh/omz-sorin/omz-sorin.zsh
+++ zsh/omz-sorin/omz-sorin.zsh
@@ -24,7 +24,7 @@ zstyle ':omz:module:terminal' auto-title 'yes'
 
 # Set the Oh My Zsh modules to load (browse modules).
 zstyle ':omz:load' omodule 'environment' 'terminal' 'editor' 'completion' \
-  'history' 'directory' 'spectrum' 'alias' 'utility' 'prompt' 'history-substring-search'
+  'history' 'directory' 'spectrum' 'utility' 'prompt' 'history-substring-search'
 
 # Set the prompt theme to load.
 # Setting it to 'random' loads a random theme.
