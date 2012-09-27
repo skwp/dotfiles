c29276bec10d73a1aaa64964e95bfb243d215b0b
Yan Pritzker <yan@pritzker.ws>
2012-09-27 13:27:40 -0700

    Clean up location of ~/.yadr

--- bin/yadr/default_libs.rb
+++ bin/yadr/default_libs.rb
@@ -1,4 +1,4 @@
 Dir[File.join(File.dirname(__FILE__),"lib/**/lib")].each {|dir| $LOAD_PATH << dir}
 require 'git-style-binary/command'
 
-$yadr = `find -L ~ -type file -maxdepth 2 -name .yadr | head | sed 's:\.yadr\/::'`.chomp
+$yadr = File.join(ENV['HOME'], ".yadr")
