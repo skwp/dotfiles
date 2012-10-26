4142651ba397002f8ce69b12f3f01f2be1ed3119
Kyle West <kswest@gmail.com>
2012-10-25 23:14:28 -0400

    removed redundant $PATH

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,3 +1,3 @@
 # path, the 0 in the filename causes this to load first
-export PATH=/usr/local/sbin:/usr/local/bin:$PATH:$HOME/.yadr/bin:$HOME/.yadr/bin/yadr
+export PATH=$PATH:$HOME/.yadr/bin:$HOME/.yadr/bin/yadr
 
