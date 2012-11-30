a16451a541a2b72a28f39838931cfa4b7e2b9b4a
Yan Pritzker <yan@pritzker.ws>
2012-11-30 13:43:46 -0600

    Removed esc-l and esc-. bindings messing with vim in zsh

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -3,9 +3,6 @@
 # http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Standard-Widgets
 
 bindkey -v                                          # Use vi key bindings
-
-bindkey -s '\el' 'ls\n'                             # [Esc-l] - run command: ls
-bindkey -s '\e.' '..\n'                             # [Esc-.] - run command: .. (up directory)
 bindkey '^r' history-incremental-search-backward    # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
 
 # emacs style
