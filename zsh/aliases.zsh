40de2c92dced35ec7a396480f7ee5e5c73169af7
Chris Fuertes <kirthar@gmail.com>
2013-12-01 11:20:03 +0100

    Customized Aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -17,6 +17,9 @@ alias psr='ps aux | grep ruby'
 
 # Moving around
 alias cdb='cd -'
+alias ago='cd ~/agorique'
+alias warfare='cd ~/med-warfare'
+alias blog='cd ~/ghost'
 
 # Show human friendly numbers and colors
 alias df='df -h'
@@ -46,61 +49,19 @@ alias ze='vim ~/.zshrc'
 alias zr='source ~/.zshrc'
 
 # Git Aliases
-alias gs='git status'
-alias gstsh='git stash'
-alias gst='git stash'
-alias gsp='git stash pop'
-alias gsa='git stash apply'
-alias gsh='git show'
-alias gshw='git show'
-alias gshow='git show'
-alias gi='vim .gitignore'
-alias gcm='git ci -m'
-alias gcim='git ci -m'
-alias gci='git ci'
-alias gco='git co'
-alias gcp='git cp'
-alias ga='git add -A'
-alias guns='git unstage'
-alias gunc='git uncommit'
-alias gm='git merge'
-alias gms='git merge --squash'
-alias gam='git amend --reset-author'
-alias grv='git remote -v'
-alias grr='git remote rm'
-alias grad='git remote add'
-alias gr='git rebase'
-alias gra='git rebase --abort'
-alias ggrc='git rebase --continue'
-alias gbi='git rebase --interactive'
-alias gl='git l'
-alias glg='git l'
-alias glog='git l'
-alias co='git co'
-alias gf='git fetch'
-alias gfch='git fetch'
-alias gd='git diff'
-alias gb='git b'
-alias gbd='git b -D -w'
-alias gdc='git diff --cached -w'
-alias gpub='grb publish'
-alias gtr='grb track'
-alias gpl='git pull'
-alias gplr='git pull --rebase'
-alias gps='git push'
+alias gco='git checkout'
+alias ga='git add'
+alias gc='git commit'
 alias gpsh='git push'
-alias gnb='git nb' # new branch aka checkout -b
-alias grs='git reset'
-alias grsh='git reset --hard'
-alias gcln='git clean'
-alias gclndf='git clean -df'
-alias gclndfx='git clean -dfx'
-alias gsm='git submodule'
-alias gsmi='git submodule init'
-alias gsmu='git submodule update'
-alias gt='git t'
-alias gbg='git bisect good'
-alias gbb='git bisect bad'
+alias gpll='git pull'
+alias grb='git rebase'
+
+# Deploy
+alias csd='cap staging deploy'
+alias csdm='cap staging deploy:migrations'
+alias cpd='cap production deploy'
+alias cpdm='cap production deploy:migrations'
+alias dsta='gco master; gpll --rebase; gco staging; grb master; gpsh'
 
 # Common shell functions
 alias less='less -r'
@@ -114,7 +75,14 @@ alias cl='clear'
 alias gz='tar -zcvf'
 
 # Ruby
-alias c='rails c' # Rails 3
+alias rs='rails server'
+alias rc='rails console'
+alias bake='bundle exec rake'
+alias routeg='bake routes | grep'
+alias rg='rails generate'
+alias rgm='rg -t model'
+alias rgmt='rg model'
+alias rgc='rg controller'
 alias co='script/console --irb=pry' # Rails 2
 alias ts='thin start'
 alias ms='mongrel_rails start'
@@ -127,10 +95,7 @@ alias k9='kill -9'
 # Gem install
 alias sgi='sudo gem install --no-ri --no-rdoc'
 
-# TODOS
-# This uses NValt (NotationalVelocity alt fork) - http://brettterpstra.com/project/nvalt/
-# to find the note called 'todo'
-alias todo='open nvalt://find/todo'
+
 
 # Forward port 80 to 3000
 alias portforward='sudo ipfw add 1000 forward 127.0.0.1,3000 ip from any to any 80 in'
@@ -146,7 +111,3 @@ alias zc='zeus console'
 alias rs='rspec spec'
 alias sr='spring rspec'
 
-# Sprintly - https://github.com/nextbigsoundinc/Sprintly-GitHub
-alias sp='sprintly'
-# spb = sprintly branch - create a branch automatically based on the bug you're working on
-alias spb="git checkout -b \`sp | tail -2 | grep '#' | sed 's/^ //' | sed 's/[^A-Za-z0-9 ]//g' | sed 's/ /-/g' | cut -d"-" -f1,2,3,4,5\`"
