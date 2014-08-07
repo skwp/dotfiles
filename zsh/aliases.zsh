e256d430cc0e107b5abc6b60d171d4e5396002d8
Yan Pritzker <yan@pritzker.ws>
2014-08-07 11:37:28 -0500

    Aliases for rails db migrations

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -161,3 +161,9 @@ alias grb='git recent-branches'
 # Finder
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+
+alias dbtp='spring rake db:test:prepare'
+alias dbm='spring rake db:migrate'
+alias dbmr='spring rake db:migrate:redo'
+alias dbmd='spring rake db:migrate:down'
+alias dbmu='spring rake db:migrate:up'
