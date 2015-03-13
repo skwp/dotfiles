17fbcc3292cc42bd1386d1b066276735f3b9afcb
Mulia Arifandi Nasution <mul14@users.noreply.github.com>
2015-03-14 00:29:11 +0700

    Make installation more faster
    Clone only the latest history

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "$HOME/.yadr" ]; then
     echo "Installing YADR for the first time"
-    git clone https://github.com/skwp/dotfiles.git "$HOME/.yadr"
+    git clone --depth=1 https://github.com/skwp/dotfiles.git "$HOME/.yadr"
     cd "$HOME/.yadr"
     [ "$1" = "ask" ] && export ASK="true"
     rake install
