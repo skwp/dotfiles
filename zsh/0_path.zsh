f5b325e5bcdf4f74395f062ab62a9296d483b75b
Forrest Phillips <forrestphillips@pairshaped.ca>
2015-04-22 14:15:57 -0400

    Added hom bin to end of path

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,2 +1,2 @@
 # path, the 0 in the filename causes this to load first
-export PATH=$PATH:$HOME/.yadr/bin:$HOME/.yadr/bin/yadr:./node_modules
+export PATH=$PATH:$HOME/.yadr/bin:$HOME/.yadr/bin/yadr:./node_modules:$HOME/bin
