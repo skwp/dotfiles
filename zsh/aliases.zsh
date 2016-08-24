79e5d1a9f4c77bc072775e1285d2aa48810defa7
Yan Pritzker <ypritzker@Yans-MacBook-Pro.local>
2016-08-24 15:13:33 -0500

    Add zzz alias for restarting zeus

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -179,6 +179,7 @@ alias zrc='zeus rails c'
 alias zrs='zeus rails s'
 alias zrdbm='zeus rake db:migrate'
 alias zrdbtp='zeus rake db:test:prepare'
+alias zzz='rm .zeus.sock; pkill zeus; zeus start'
 
 # Rspec
 alias rs='rspec spec'
