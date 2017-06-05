17d8b5e15c47fc407ca858858d49b5ba3696f9ae
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2017-06-05 23:02:39 +0200

    Prevent duplicate entries in $PATH. Fixes #690

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,7 +1,14 @@
 # path, the 0 in the filename causes this to load first
-path=(
-  $path
-  $HOME/.yadr/bin
-  $HOME/.yadr/bin/yadr
-)
+#
+# If you have duplicate entries on your PATH, run this command to fix it:
+# PATH=$(echo "$PATH" | awk -v RS=':' -v ORS=":" '!a[$1]++{if (NR > 1) printf ORS; printf $a[$1]}')
 
+pathAppend() {
+  # Only adds to the path if it's not already there
+  if ! echo $PATH | egrep -q "(^|:)$1($|:)" ; then
+    PATH=$PATH:$1
+  fi
+}
+
+pathAppend "$HOME/.yadr/bin"
+pathAppend "$HOME/.yadr/bin/yadr"
