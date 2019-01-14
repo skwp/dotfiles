526a71ffca7d9139a924a77722c78b5635d3c13f
Jonas Bygdén <github@bygden.nu>
2019-01-14 16:13:37 +0100

    Removed 'brew prune' to get rid of deprecation warning

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -210,4 +210,4 @@ alias dbmd='spring rake db:migrate:down'
 alias dbmu='spring rake db:migrate:up'
 
 # Homebrew
-alias brewu='brew update  && brew upgrade && brew cleanup && brew prune && brew doctor'
+alias brewu='brew update  && brew upgrade && brew cleanup && brew doctor'
