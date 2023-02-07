b71de4feb1d68918cfae150a968744fd6f455d93
Ashok Raju <deville75@gmail.com>
2023-02-06 21:52:48 -0500

    Update install.sh with my github dotfiles

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [ ! -d "$HOME/.yadr" ]; then
-    echo "Installing YADR for the first time"
-    git clone --depth=1 https://github.com/skwp/dotfiles.git "$HOME/.yadr"
+    echo "Installing Ashok's YADR for the first time"
+    git clone --depth=1 https://github.com/rajuashok/dotfiles.git "$HOME/.yadr"
     cd "$HOME/.yadr"
     [ "$1" = "ask" ] && export ASK="true"
     rake install
 else
-    echo "YADR is already installed"
+    echo "Ashok's YADR is already installed"
 fi
